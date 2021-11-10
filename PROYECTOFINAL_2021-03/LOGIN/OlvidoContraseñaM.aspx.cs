272a27e8ce0eb361c28e55bb741949b7dba581d5
Ashley Helago <ashley.hnlago@uao.edu.co>
2021-11-09 22:10:31 -0500

    Changes and create metod DatabBase Login and Change password

--- PROYECTOFINAL_2021-03/LOGIN/OlvidoContraseñaM.aspx.cs
+++ PROYECTOFINAL_2021-03/LOGIN/OlvidoContraseñaM.aspx.cs
@@ -14,9 +14,37 @@ namespace PROYECTOFINAL_2021_03.LOGIN
 
         }
 
-        protected void btCambiarContraseña_Click(object sender, EventArgs e)
+        protected void btCambiarContraseñak(object sender, EventArgs e)
         {
-            if (tbxUsuarioC.Text=="" && tbxContraseñaC.Text == "") { tbxErrorCambioC.Text="Error Ingresar Usuario y/o Contraseña*";}
+            var sqlConn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConexionSQL"].ConnectionString);
+            var strSQL = "UPDATE Estudiante SET [Contraseña] WHERE [CorreoElectronico] = '" + txbUsuarioC.Text + "'";
+            var cmd = new SqlCommand(strSQL, sqlConn);
+            var ds = new DataSet();
+            var da = new SqlDataAdapter(cmd);
+
+            sqlConn.Open();
+            da.Fill(ds, "Estudiante");
+            sqlConn.Close();
+            var dt = ds.Tables[0];
+            txbContraseñaC.Text = dt.Rows[0][1].ToString();
+            if (txbContraseñaC.Text=="" && tbxContraseñaC.Text == "") { tbxErrorCambioC.Text="Error Ingresar Usuario y/o Contraseña*";}
+            Limpiar()
         }
+
+        public void Limpiar()
+        {
+            txb_Cedula.Text = string.Empty;
+            txb_Nombre.Text = string.Empty;
+            txb_Apellido.Text = string.Empty;
+            txb_Edad.Text = string.Empty;
+            txb_Telefono.Text = string.Empty;
+        }
+
+        protected void BTLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+
     }
 }
--- PROYECTOFINAL_2021-03/LOGIN/RegistrarseM.aspx.cs
+++ PROYECTOFINAL_2021-03/LOGIN/RegistrarseM.aspx.cs
@@ -13,5 +13,30 @@ namespace PROYECTOFINAL_2021_03.LOGIN
         {
 
         }
+
+        protected void btEnviarRegistro(object sender, EventArgs e)
+        {
+            var sqlConn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConexionSQL"].ConnectionString);
+            var strSQL = "INSERT INTO[dbo].[Estudiante]([CodigoEstudiante],[NombreEstudiante1],[NombreEstudiante2],[ApellidoEstudiante1],[ApellidoEstudiante2],[Telefono1],[Telefono2],[Direccion],[CorreoElectronico],[Contraseña],[Semestre]) VALUES ('" + txbCodigo.Text + "','" + txbPNombre.Text + "','" + txbSNombre.Text + "','" + txbPApellido.Text + "','" + txbSApellido.Text + "','" + txbTelefono1.Text + "','" + "','" + txbTelefono2.Text + "','" +  "','" + txbDireccion.Text + "','" + "','" + txbCorreoElectronico.Text + "','" + "','" + txbContraseña.Text + "','" + "','" + txbSemestre.Text + "')";
+            var cmd = new SqlCommand(strSQL, sqlConn);
+            sqlConn.Open();
+            cmd.ExecuteNonQuery();
+            sqlConn.Close();
+            Limpiar();
+        }
+
+        public void Limpiar()
+        {
+            txb_Cedula.Text = string.Empty;
+            txb_Nombre.Text = string.Empty;
+            txb_Apellido.Text = string.Empty;
+            txb_Edad.Text = string.Empty;
+            txb_Telefono.Text = string.Empty;
+        }
+
+        protected void BTLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
     }
 }
